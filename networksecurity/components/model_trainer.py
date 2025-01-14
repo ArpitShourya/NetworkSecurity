bb9a29018a25788454e4e4ae741a1aa59b42a23e
Arpit Shourya <arpitshourya2233@gmail.com>
2025-01-14 11:33:38 +0530

    Pipeline Completed

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -20,6 +20,7 @@ from sklearn.ensemble import (
 import mlflow
 from urllib.parse import urlparse
 import dagshub
+dagshub.init(repo_owner='ArpitShourya', repo_name='NetworkSecurity', mlflow=True)
 
 class ModelTrainer:
     def __init__(self,model_trainer_config:ModelTrainerConfig,data_transformation_artifact:DataTransformationArtifact):
@@ -102,6 +103,7 @@ class ModelTrainer:
 
         Network_Model=NetworkModel(preprocessor=preprocessor,model=best_model)
         save_object(self.model_trainer_config.trained_model_file_path,obj=NetworkModel)
+        save_object("final_model/model.pkl",best_model)
 
         model_trainer_artifact=ModelTrainerArtifact(trained_model_file_path=self.model_trainer_config.trained_model_file_path,
                              train_metric_artifact=classification_train_metric,
